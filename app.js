ff0b544696cb97cf9e5fe0da2e87ad8b881b0179
Gabriel López <gabolopez1990@gmail.com>
2018-10-16 16:30:00 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 var express = require("express");
 var app = express();
+const bodyParser = require("body-parser");
+var db = require("./data.js");
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
+app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", (req,res,err)=>{
-  if(err){
-    retur err;
-  }
-  res.send(req.params);
+app.post("/", urlencodedParser, (req,res,next)=>{
+	var nac = req.body.nac;
+	var ci = req.body.ci;
+
+	if(ci.length<8){
+		while(ci.length<8){
+			ci = "0"+ci;
+		}
+	}	
+	var cedula = nac + ci;	
+	// var empleado = db.find(o => o.CEDULA == cedula);
+	var empleado = db.find((o) =>{
+		return o.CEDULA == cedula;
+	});
+
+	if(empleado === undefined){
+		res.send("Cedula no encontrada");
+	}
+	else{
+		res.send(empleado);	
+	}
 });
+			
 
 app.listen(8080);
 console.log("listening on port "+ 8080);
